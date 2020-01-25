c0dbb6f18951e9cd21234c2e532b497ffd75d84e
Hannah Woodward <hannah@serieseight.com>
2020-01-25 00:39:36 +0000

    Add lazy picture element support

--- src/uncloak-item.js
+++ src/uncloak-item.js
@@ -17,7 +17,7 @@ export class UncloakItem {
     this.delayType = node.getAttribute('data-uncloak-delay-type') || null
     this.delayTypes = options.delayTypes || {}
     this.instance = instance
-    this.lazyContent = node.hasAttribute('data-uncloak-ignore-lazy') ? [] : node.querySelectorAll('[data-uncloak-src], [data-uncloak-srcset]')
+    this.lazyContent = node.hasAttribute('data-uncloak-ignore-lazy') ? [] : node.querySelectorAll('[data-uncloak-src], [data-uncloak-srcset], [data-uncloak-picture]')
     this.lazyContentLoadStatus = (this.lazyContent[0] ? -1 : 2) // NB: -1 => unloaded, 1 => loading, 2 => loaded
     this.node = node
     this.threshold = parseFloat(node.getAttribute('data-uncloak-threshold')) || 0
@@ -125,14 +125,19 @@ export class UncloakItem {
     if (this.mediaLoaded() || this.lazyContentLoadStatus === 1) {
       return
     }
+
     this.lazyContentLoadStatus = 1
     let left_to_load = this.lazyContent.length
 
-    const loaded = (element) => {
+    const loaded = (root_element, listener_element) => {
       return () => {
-        element.removeEventListener('load', loaded, false)
-        element.removeAttribute('data-uncloak-src')
-        element.removeAttribute('data-uncloak-srcset')
+        listener_element.removeEventListener('load', loaded, false)
+        root_element.removeAttribute('data-uncloak-src')
+        root_element.removeAttribute('data-uncloak-srcset')
+        root_element.removeAttribute('data-uncloak-ie-src')
+        root_element.removeAttribute('data-uncloak-alt')
+        root_element.removeAttribute('data-uncloak-class')
+        root_element.removeAttribute('data-uncloak-picture')
         left_to_load -= 1
         if (left_to_load === 0) {
           this.lazyContentLoadStatus = 2
@@ -146,13 +151,35 @@ export class UncloakItem {
     for (let i = 0; i < this.lazyContent.length; i++) {
       const el = this.lazyContent[i]
       const lazy_srcset = el.getAttribute('data-uncloak-srcset') || null
+      let listener_el = el
+
       if (lazy_srcset) {
         el.srcset = lazy_srcset
       }
       if (el.hasAttribute('data-uncloak-src')) {
         el.src = el.getAttribute('data-uncloak-src')
       }
-      el.addEventListener('load', loaded(el), false)
+
+      if (el.hasAttribute('data-uncloak-picture')) {
+        const img = document.createElement('img')
+        listener_el = img
+
+        if (this.instance.isIE && el.hasAttribute('data-uncloak-ie-src')) {
+          img.src = el.getAttribute('data-uncloak-ie-src')
+        }
+
+        if (el.hasAttribute('data-uncloak-alt')) {
+          img.alt = el.getAttribute('data-uncloak-alt')
+        }
+
+        if (el.hasAttribute('data-uncloak-class')) {
+          img.classList = el.getAttribute('data-uncloak-class')
+        }
+
+        el.append(img)
+      }
+
+      listener_el.addEventListener('load', loaded(el, listener_el), false)
     }
   }
   mediaLoaded() {
